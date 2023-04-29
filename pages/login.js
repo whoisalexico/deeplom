8cdd1cffaee246ae55381b96e3989996c1238034
nVolosevichh <nvolosevich@itwconsulting.com>
2023-04-29 17:56:51 +0300

    working on

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {useAuth} from "../context/AuthContext";
-import {router} from "next/client";
+import {useRouter} from "next/router";
 import Link from "next/link";
 import {TextField} from "@mui/material";
 import s from '../styles/Auth.module.scss'
@@ -10,6 +10,7 @@ import Navbar from "../components/layout/navbar";
 const Login = () => {
     const {user, login} = useAuth();
     console.log(user)
+    const router = useRouter();
     const [data, setData] = useState({
         email: '',
         password: '',
