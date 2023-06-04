ea7b7269aa68d0f53eb5433a854a871fe9aadfe4
whoisalexico <axux228@gmail.com>
2023-06-04 20:50:03 +0300

    working on

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAp1_4x5CPs3j0te1lftU8EXxjcKDMPOaI",
-    authDomain: "deeplom-project.firebaseapp.com",
-    projectId: "deeplom-project",
-    storageBucket: "deeplom-project.appspot.com",
-    messagingSenderId: "588006456613",
-    appId: "1:588006456613:web:c2076f3954490d83cdaecc"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_API_KEY,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
